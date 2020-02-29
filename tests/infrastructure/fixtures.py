aeab9113cd5e94b4b28b57a1043dfacacfdea539
Kale Miller <kale.miller96@gmail.com>
2020-02-29 18:00:19 +1100

    Added start of infrastructure tests

--- tests/infrastructure/fixtures.py
+++ tests/infrastructure/fixtures.py
@@ -1,18 +1,19 @@
 import pytest
 
+from python_terraform import Terraform
+
 from infrastructure.utils import database
 
+tf = Terraform(working_dir='infrastructure/')
+
 @pytest.fixture
-def infrastructure(data: bool):
+def infrastructure(data: bool = True):
     """Stands up a fresh copy of the Terraform infrastructure to work with."""
-    ...
+    tf.apply()
     if data:
         fill_database(db, sample_path="../test-data/small.csv")
-    ...
-    raise NotImplementedError()
+    
+    yield tf
 
-
-@pytest.fixture
-def data():
-    """Fills the DynamoDB database with dummy data."""
-    raise NotImplementedError()
+    tf.destroy()
+    return 
