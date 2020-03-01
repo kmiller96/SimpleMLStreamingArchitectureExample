dc52ff807d6afb221fef245198611438cd778107
Kale Miller <kale.miller96@gmail.com>
2020-03-01 14:47:23 +1100

    Added reader lambda source code

--- lambdas/reader/app.py
+++ lambdas/reader/app.py
@@ -1,2 +1,26 @@
-def lambda_handler(event, context):
-    return 200
+"""Pulls changed items from DynamoDB table and pushes them into a queue."""
+import boto3
+import os
+
+from . import utils 
+
+QUEUE_URL = os.environ['QUEUE_URL']
+
+sqs = boto3.client('sqs')
+
+def lambda_handler(event, context=None):
+    table_items = utils.parse_dynamodb_event(event)
+
+    responses = []
+    for item in table_items:
+        queue_message = utils.format_message(item)
+        response = sqs.send_message(
+            QueueUrl = QUEUE_URL,
+            MessageBody = queue_message,
+        )
+        responses.append(response)
+
+    return {
+        "Status": 200,
+        "Records": responses
+    }
---
+++ lambdas/reader/utils/parse.py
@@ -0,0 +1,20 @@
+def parse_dynamodb_event(event):
+    items = []
+
+    for record in event['Records']:
+        dynamodb_item = record['dynamodb']['NewImage']
+        pandas_item = dynamodb_to_pandas(dynamodb_item)
+        items.append(pandas_item)
+    
+    return items
+
+
+def dynamodb_to_pandas(item):
+    pandas_row = {
+        col: (
+            r.get('S') or 
+            r.get('N') or 
+            r.get('B')
+        ) for col, r in item.items()
+    }
+    return pandas_row
