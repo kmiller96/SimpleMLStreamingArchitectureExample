19ed744244cac104415fe0200f97a09e8f09b6f8
Kale Miller <kale.miller96@gmail.com>
2020-04-14 10:53:23 +1000

    Added AWS profile to filling utility

--- scripts/fill_dynamodb.py
+++ scripts/fill_dynamodb.py
@@ -8,57 +8,57 @@ import pandas as pd
 import boto3
 from tqdm import tqdm
 
-DYNAMODB_TABLE_NAME = 'real-time-wine-vat-data'
+DYNAMODB_TABLE_NAME = "real-time-wine-vat-data"
 
 
 @click.command()
-@click.option('-n', '--entries', type=int, default=200)
-def main(entries):
+@click.option("-n", "--entries", type=int, default=200)
+@click.option("--profile", default="default")
+def main(entries, profile):
     try:
-        df = load_data(fpath=os.path.join(os.getcwd(), 'data', 'full.csv'))
+        session = boto3.Session(profile_name=profile)
+
+        df = load_data(fpath=os.path.join(os.getcwd(), "data", "full.csv"))
         data = format_dataframe(df, n=entries)
-        push_data_to_dynamodb(data)
+
+        push_data_to_dynamodb(data, session=session)
     except KeyboardInterrupt as e:
         print("Stopping data push...")
     finally:
-        log_results()
+        log_results(session=session)
     return
 
 
 def load_data(fpath):
-    df = pd.read_csv(fpath, sep=';')
-    df.index.rename('vatID', inplace=True)
+    df = pd.read_csv(fpath, sep=";")
+    df.index.rename("vatID", inplace=True)
     df.reset_index(inplace=True)
-    df = df.astype('str')
-    df.vatID = pd.util.hash_array(df.vatID).astype('str')
+    df = df.astype("str")
+    df.vatID = pd.util.hash_array(df.vatID).astype("str")
     return df
 
 
 def format_dataframe(df, n):
     rows = df.shape[0]
     dff = df.sample(min(n, rows))  # Either get entire df or get n.
-    return dff.to_dict(orient='records')
+    return dff.to_dict(orient="records")
 
 
-def push_data_to_dynamodb(data):
-    dynamodb = boto3.resource('dynamodb')
+def push_data_to_dynamodb(data, session):
+    dynamodb = session.resource("dynamodb")
     table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
-    with table.batch_writer() as batch:
+    with DYNAMODB_TABLE.batch_writer() as batch:
         for entry in tqdm(data):
-            batch.put_item(
-                Item = entry
-            )
+            batch.put_item(Item=entry)
     return
 
 
-def log_results():
-    dynamodb = boto3.resource('dynamodb')
+def log_results(session):
+    dynamodb = session.resource("dynamodb")
     table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
-    scan_results = table.scan(
-        Select = "COUNT",
-    )
+    scan_results = DYNAMODB_TABLE.scan(Select="COUNT",)
 
     print("\n== RESULTS ==")
     print(f"Current total entries in table: {scan_results['Count']}")
