0ae9bcc84570beb8650d296af2f7768d907448f0
Kale Miller <kale.miller96@gmail.com>
2020-04-14 18:36:22 +1000

    Added newly required range key to output of writer lambda

--- lambdas/writer/utils/format.py
+++ lambdas/writer/utils/format.py
@@ -1,4 +1,11 @@
+from datetime import datetime
+import time
+
+
 def format_dynamodb_key(entry):
     """Converts from Pandas format to DynamoDB format."""
     item = {k: str(v) for k, v in entry.items()}
+
+    item["timestamp_in_seconds"] = time.time()
+    item["inference_date"] = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
     return item
