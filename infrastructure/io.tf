2967704d5b07e42272c7244d4c8c4259305dd021
Kale Miller <kale.miller96@gmail.com>
2020-04-14 11:12:11 +1000

    Minor refactor of Terraform source

---
+++ infrastructure/io.tf
@@ -0,0 +1,39 @@
+###############
+## Variables ##
+###############
+
+variable "resource_prefix" {
+  type = string
+}
+
+variable "source_code_bucket" {
+  type = string
+}
+
+variable "inference_lambda_source_key" {
+  type = string
+}
+
+variable "writer_lambda_source_key" {
+  type = string
+}
+
+variable "reader_lambda_source_key" {
+  type = string
+}
+
+#############
+## Outputs ##
+#############
+
+output "dynamodb_table" {
+  value = module.database.table
+}
+
+output "reader_queue_url" {
+  value = module.reader_queue.url
+}
+
+output "writer_queue_url" {
+  value = module.writer_queue.url
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -52,3 +52,11 @@ module "database" {
   name            = "vat-data"
   description     = "Data store for the IoT data. Chosen for quick read/writes."
 }
+
+resource "aws_lambda_event_source_mapping" "dynamodb" {
+  depends_on = [module.reader_lambda, module.database]
+
+  event_source_arn  = module.database.stream_arn
+  function_name     = module.reader_lambda.function_arn
+  starting_position = "LATEST"
+}
